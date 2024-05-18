358d85ae55b3770ee1c1e5b0bfc4f3067ff599eb
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-18 13:18:44 +0300

    .

--- redux/actionCreators.js
+++ redux/actionCreators.js
@@ -1,17 +1,15 @@
-import getCurrentTime from "../utils/getCurrentTime";
-import * as actionTypes from "./actionTypes";
+import getCurrentTime from "../utils/getCurrentTime.js";
+import * as actionTypes from "./actionTypes.js";
 
-function addCurrentTime() {
+export function addCurrentTime() {
   return {
     type: actionTypes.ADD_CURRENT_TIME,
     payload: getCurrentTime(),
   };
 }
 
-function clearTimes() {
+export function clearTimes() {
   return {
     type: actionTypes.CLEAR_ALL_TIMES,
   };
 }
-
-export { addCurrentTime, clearTimes };
